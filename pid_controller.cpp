19cf969269132a95ed8f83de3ec52da009496d9b
johnsozm <z.m.johnson@protonmail.com>
2022-08-17 13:12:45 -0600

    Added note on possible change in numerical methods

--- pid_controller.cpp
+++ pid_controller.cpp
@@ -13,12 +13,13 @@ void PIDController::initialize(double _target, double _state, double _timestamp)
 
 double PIDController::calculateOutput(double state, double timestamp) {
     //Calculate numerical derivative and integral
+    //TODO: Is this guaranteed to be polled at a regular interval? Could use finite differences here if so
     double error = target - state;
     double dt = timestamp - last_timestamp;
     double error_derivative = (error - last_error) / dt;
     error_integral += ((error + last_error) / 2) * dt;
 
-    //Calculate response and update log parameters
+    //Calculate response and update log variables
     double response = params.k_p * error + params.k_d * error_derivative + params.k_i * error_integral;
     last_timestamp = timestamp;
     last_error = error;
