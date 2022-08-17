042e5dfd10ff978858f84b4896dbdc0ebe4803bb
johnsozm <z.m.johnson@protonmail.com>
2022-08-17 12:05:37 -0600

    Implemented test cases

--- pid_tester.cpp
+++ pid_tester.cpp
@@ -1,19 +1,160 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
 #include "pid_controller.h"
 
+using namespace std;
+
 void testStepFunction() {
-    //TODO: Implement
+	//Write file header
+    ofstream out;
+    out.open("test_results/step_function.csv");
+    out << "Time,Target,State" << endl;
+    
+    PIDParameters params {
+    	k_p: 5,
+    	k_i: 5,
+    	k_d: 5
+    };
+    
+    double t = 0;
+    double dt = 0.01;
+    double state = 0;
+    double velocity = 0;
+    double target = 0;
+    
+    PIDController controller(params, target, t, state);
+    
+    for (int i = 0; i < 1000; i++) {
+    	t += dt;
+    	if (i == 100) {
+    		target = 1;
+    	}
+    	controller.updateTarget(target);
+    	double output = controller.calculateOutput(state, t);
+    	velocity += output * dt;
+    	state += velocity * dt;
+    	
+    	out << t << "," << target << "," << state << endl;
+    }
+     
+    out.close();
 }
 
 void testSquareWave() {
-    //TODO: Implement
+    //Write file header
+    ofstream out;
+    out.open("test_results/square_wave.csv");
+    out << "Time,Target,State" << endl;
+    
+    PIDParameters params {
+    	k_p: 5,
+    	k_i: 5,
+    	k_d: 5
+    };
+    
+    double t = 0;
+    double dt = 0.01;
+    double state = 0;
+    double velocity = 0;
+    double target = 0;
+    
+    PIDController controller(params, target, t, state);
+    
+    for (int i = 0; i < 10000; i++) {
+    	t += dt;
+    	if (i % 1000 == 0) {
+    		target = 1 - target;
+    	}
+    	controller.updateTarget(target);
+    	double output = controller.calculateOutput(state, t);
+    	velocity += output * dt;
+    	state += velocity * dt;
+    	
+    	out << t << "," << target << "," << state << endl;
+    }
+     
+    out.close();
 }
 
 void testUnevenSquareWave() {
-    //TODO: Implement
+    //Write file header
+    ofstream out;
+    out.open("test_results/uneven_square_wave.csv");
+    out << "Time,Target,State" << endl;
+    
+    PIDParameters params {
+    	k_p: 5,
+    	k_i: 5,
+    	k_d: 5
+    };
+    
+    double t = 0;
+    double dt = 0.01;
+    double state = 0;
+    double velocity = 0;
+    double target = 0;
+    
+    PIDController controller(params, target, t, state);
+    
+    for (int i = 0; i < 10000; i++) {
+    	t += dt;
+    	if (i % 1000 == 0) {
+    		if (target > 0) {
+    			target = 0;
+    		}
+    		else {
+    			if (i % 4000 == 0) {
+    				target = 0.5;
+    			}
+    			else {
+    				target = 1.0;
+    			}
+    		}
+    	}
+    	controller.updateTarget(target);
+    	double output = controller.calculateOutput(state, t);
+    	velocity += output * dt;
+    	state += velocity * dt;
+    	
+    	out << t << "," << target << "," << state << endl;
+    }
+     
+    out.close();
 }
 
 void testSinusoid() {
-    //TODO: Implement;
+    //Write file header
+    ofstream out;
+    out.open("test_results/sinusoid.csv");
+    out << "Time,Target,State" << endl;
+    
+    PIDParameters params {
+    	k_p: 5,
+    	k_i: 5,
+    	k_d: 5
+    };
+    
+    double t = 0;
+    double dt = 0.01;
+    double state = 0;
+    double velocity = 0;
+    double target = 0;
+    
+    PIDController controller(params, target, t, state);
+    
+    for (int i = 0; i < 1000; i++) {
+    	t += dt;
+    	target = sin(t * 6.283185);
+    	controller.updateTarget(target);
+    	double output = controller.calculateOutput(state, t);
+    	velocity += output * dt;
+    	state += velocity * dt;
+    	
+    	out << t << "," << target << "," << state << endl;
+    }
+     
+    out.close();
 }
 
 int main() {
