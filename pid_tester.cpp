034e6672d12abcddfb8e8bcacc1f2f26892ce15e
johnsozm <z.m.johnson@protonmail.com>
2022-08-17 10:40:26 -0600

    Added placeholder test functions

--- pid_tester.cpp
+++ pid_tester.cpp
@@ -1,5 +1,25 @@
 #include "pid_controller.h"
 
+void testStepFunction() {
+    //TODO: Implement
+}
+
+void testSquareWave() {
+    //TODO: Implement
+}
+
+void testUnevenSquareWave() {
+    //TODO: Implement
+}
+
+void testSinusoid() {
+    //TODO: Implement;
+}
+
 int main() {
+    testStepFunction();
+    testSquareWave();
+    testUnevenSquareWave();
+    testSinusoid();
     return 0;
 }
